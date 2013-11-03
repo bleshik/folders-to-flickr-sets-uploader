1e4b03fb63e1f722c35d99acecaa70e4eef3b52a
Alexey Balchunas <bleshik@gmail.com>
2013-11-03 15:45:29 +0400

    avoiding several processes

--- upload-sets-in-the-folder.sh
+++ upload-sets-in-the-folder.sh
@@ -17,6 +17,14 @@
 #      REVISION:  ---
 #===============================================================================
 
+# 
+PID_FILE="/var/tmp/`basename $0`.pid"
+touch "$PID_FILE"
+read PID < "$PID_FILE"
+# if PID is not null and a process with that pid exists, exit
+[ ! -z "`ps -p $PID | sed -n 2p`" ] && echo "Running already..." && exit 0
+echo $$ > "$PID_FILE"
+
 echo Uploading all sets in the folder \"$1\"
 cd $1
 for folder in `ls` ; do
