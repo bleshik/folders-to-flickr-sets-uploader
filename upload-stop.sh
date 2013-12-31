acf683f3b8d23d382e02cd8aec9117e60ce19e08
Alexey Balchunas <bleshik@yandex-team.ru>
2013-12-31 11:29:57 +0400

    added script killing all the uploading script processes

---
+++ upload-stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: upload-stop.sh
+# 
+#         USAGE: ./upload-stop.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Alexey Balchunas, 
+#  ORGANIZATION: 
+#       CREATED: 12/31/2013 11:28:47 MSK
+#      REVISION:  ---
+#===============================================================================
+for i in `ps aux | grep flickr | grep -v grep | tr -s ' ' | cut -f2 -d ' '` ; do echo $i ; kill $i ; done
+
